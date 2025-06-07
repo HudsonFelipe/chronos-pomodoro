800dcd2aa4f2d3cdc93bab036eff50e678a3a4e8
Hudson Felipe <hudson0211@gmail.com>
2025-06-07 15:25:05 +0000

    feat: add CountDown component with styles and integrate it into App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "../src/styles/global.css";
 import "../src/styles/theme.css";
 
 import { Container } from "./components/Container";
+import { CountDown } from "./components/CountDown";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
@@ -14,6 +15,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
